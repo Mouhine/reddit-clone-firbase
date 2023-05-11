7746f3c1d272f19462677ca9ffaee5081bd2c79e
mohammed mouhine <mouhinemohmade@gmail.com>
2023-05-11 10:36:04 +0100

    fix layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/clientApp";
 import useAuth from "../../hooks/useAuth";
 import Navbar from "../Navbar";
 import AuthModal from "../Modal/Auth";
 
-const Layout: React.FC = ({ children }) => {
+const Layout = ({ children }: { children: ReactNode }) => {
   // useAuth(); // will implement later at end of tutorial
 
   return (
